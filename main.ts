b72550f045d7eaa0525135d3547f1bb819e69e96
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2022-07-26 21:59:25 +0100

    add single jacdac button

--- main.ts
+++ main.ts
@@ -1,8 +1,7 @@
-input.onButtonPressed(Button.A, function () {
-    radio.sendNumber(randint(0, 9))
-})
 basic.showIcon(IconNames.Ghost)
 radio.setGroup(42)
 basic.forever(function () {
-	
+    if (modules.button1.pressed()) {
+        radio.sendNumber(randint(0, 9))
+    }
 })
