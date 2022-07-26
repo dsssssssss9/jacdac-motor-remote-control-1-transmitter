ef393beaf250478dda8ff3be50dbeb5e05a6b1df
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2022-07-26 22:04:46 +0100

    add 2nd button send test string

--- main.ts
+++ main.ts
@@ -1,6 +1,9 @@
 modules.button1.onEvent(jacdac.ButtonEvent.Down, function () {
     radio.sendNumber(randint(0, 9))
 })
+modules.button2.onEvent(jacdac.ButtonEvent.Down, function () {
+    radio.sendString("Frank")
+})
 basic.showIcon(IconNames.Ghost)
 radio.setGroup(42)
 basic.forever(function () {
