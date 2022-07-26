96bf7fbbb72ae0f692a54b4407e37f8b44bf74d5
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2022-07-26 23:43:58 +0100

    add comments

--- main.ts
+++ main.ts
@@ -1,17 +1,36 @@
+// 
+// If Jacdac Button 1 is pressed down send 1 to receiver
+// 
+// This value determines direction of Motor
 modules.button1.onEvent(jacdac.ButtonEvent.Down, function () {
     radio.sendNumber(1)
 })
-modules.potentiometer1.onPositionChangedBy(5, function () {
-    Speed = modules.potentiometer1.position()
-    radio.sendValue("Speed", Speed)
-})
+// 
+// If Jacdac Button 2 is pressed down send -1 to receiver
+// 
+// This value determines direction of Motor
 modules.button2.onEvent(jacdac.ButtonEvent.Down, function () {
     radio.sendNumber(-1)
 })
-let Speed = 0
+// Show Icon on Micro:Bit to show system is ready
+// 
+// Set radio Group to match that of receiver
+// 
+// Set Variavle "Speed" to 0 - this is used to control the speed of the motor connected to the receiver
+// 
+// 
 basic.showIcon(IconNames.Ghost)
 radio.setGroup(42)
-Speed = 0
+let Speed = 0
+// Set Variable "Speed" to value of device used to control speed of motor.
+// 
+// Send this value via radio with a name of "Speed" 
+// 
+// In this case we are using a Jacdac Slide Potentiometer 
+// 
+// Other devices that give an analog output ( rotary Potentiometer etc ) 
+// could also be used
 basic.forever(function () {
-	
+    Speed = Math.ceil(modules.potentiometer1.position())
+    radio.sendValue("Speed", Speed)
 })
