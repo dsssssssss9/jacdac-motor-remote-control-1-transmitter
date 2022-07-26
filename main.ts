4ba008cdab97d6698e8d21ab05429ddbbcdd43c0
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2022-07-26 22:13:32 +0100

    test Jac buttons to set direction. Slider for speed

--- main.ts
+++ main.ts
@@ -1,11 +1,17 @@
 modules.button1.onEvent(jacdac.ButtonEvent.Down, function () {
-    radio.sendNumber(randint(0, 9))
+    radio.sendNumber(1)
+})
+modules.potentiometer1.onPositionChangedBy(5, function () {
+    Speed = modules.potentiometer1.position()
+    radio.sendValue("Speed", Speed)
 })
 modules.button2.onEvent(jacdac.ButtonEvent.Down, function () {
-    radio.sendString("Frank")
+    radio.sendNumber(-1)
 })
+let Speed = 0
 basic.showIcon(IconNames.Ghost)
 radio.setGroup(42)
+Speed = 0
 basic.forever(function () {
 	
 })
