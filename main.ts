9fe2a123203793ba2ebba0d4418a98aabbc55720
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2022-07-26 22:02:16 +0100

    use on button down

--- main.ts
+++ main.ts
@@ -1,7 +1,8 @@
+modules.button1.onEvent(jacdac.ButtonEvent.Down, function () {
+    radio.sendNumber(randint(0, 9))
+})
 basic.showIcon(IconNames.Ghost)
 radio.setGroup(42)
 basic.forever(function () {
-    if (modules.button1.pressed()) {
-        radio.sendNumber(randint(0, 9))
-    }
+	
 })
